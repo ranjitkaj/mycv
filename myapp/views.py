a925b0dbebbc6fef68a38f64b8dc1f8699e7ec47
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2023-09-01 07:56:21 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-from .models import myform
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import CreateUserForm
+
+
+def todoapp(request):
+    if request.method == "POST":
+        b = CreateUserForm(request.POST)
+        if b.is_valid():
+            b.save()
+    b = CreateUserForm()
+    c = todo.objects.all()
+    return render(request, "todo.html", {"form":b, "data":c})
+
+
+def delete(request, id):
+    d = todo.objects.get(id=id)
+    d.delete()
+    return redirect("todo")
 
 # Create your views here.
 def index(request):
